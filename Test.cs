345efee023c018c82e96a145170145046bdac235
Evgeniy <Murmandir@mail.ru>
2022-09-03 19:29:51 +0500

    Вывел в консоль колличество элементов массива

--- Test.cs
+++ Test.cs
@@ -1,10 +1,18 @@
 void task1()
 {
-    string[] words = {"Hello ", "world ", "2 "};
-    for (int i = 0; i < words.Length; i++)
-    {
-        Console.Write(words[i]);
-    }
+    string[] words = {"Hello ", "world ", "2 ","AAA"};
+        {
+            for (int i = 0; i < words.Length; i++)
+                {
+                    Console.Write(words[i]);
+                
+                }
+                Console.WriteLine("");
+        }
+    
+    string[] solution = words;
+    int length = solution.Length;
+    Console.WriteLine("Колличество Элементов = " + length);
 
 }
 task1();
