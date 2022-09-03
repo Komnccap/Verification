7e231a1bab8e8b7624e1a1a8341b54b9668bbc04
Evgeniy <Murmandir@mail.ru>
2022-09-03 20:00:57 +0500

    создал массив из массива с формированием строк в которых не более трёх символов

--- Test.cs
+++ Test.cs
@@ -1,18 +1,28 @@
 void task1()
 {
-    string[] words = {"Hello ", "world ", "2 ","AAA"};
+    string[] words = {"Hello ", "world ", "2 ","AAA "};
         {
             for (int i = 0; i < words.Length; i++)
                 {
                     Console.Write(words[i]);
                 
                 }
-                Console.WriteLine("");
+                
         }
+        Console.WriteLine("");
+        Console.WriteLine("");
     
     string[] solution = words;
-    int length = solution.Length;
-    Console.WriteLine("Колличество Элементов = " + length);
-
+        for (int i = 0; i < words.Length; i++)
+                {
+                    int length = solution[i].Length - 1;
+                    if (length <= 3)
+                    {
+                        Console.WriteLine("Слова в которых не больше трёх символов - " + words[i]);
+                        Console.WriteLine("");
+                    }
+                    
+               }
+   
 }
 task1();
