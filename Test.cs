ad2624baf86ad4363aeb7576d0097c3c04d437df
Evgeniy <Murmandir@mail.ru>
2022-09-03 21:49:09 +0500

    Полностью изменил решение

--- Test.cs
+++ Test.cs
@@ -1,45 +1,44 @@
 void task1()
 {
     int counter = 0;
-    string var ="";
-    string[] words = {"Hello ", "world ", "QA ","AAA "};
-        {
-            for (int i = 0; i < words.Length; i++)
-                {
-                    Console.Write(words[i]);
+    string[] words = {"Hello ", "world ", "QA ", "AAA "};
+
+    for(int i = 0; i < words.Length; i++)
+    {
+        Console.Write(words[i]);
+             
+    }
+    Console.WriteLine("");
+    Console.WriteLine("");
+
+    for (int i = 0; i < words.Length; i++)
+    {  
+        int length = words[i].Length - 1;
+        if (length <= 3)
+        {                        
+            Console.WriteLine("Слова в которых не больше трёх символов - " + words[i]);
+            counter++;                        
+        }  
+    }
                 
-                }
-                
-        }
-        Console.WriteLine("");
-        Console.WriteLine("");
-    
-    string[] examination = words;
-        for (int i = 0; i < words.Length; i++)
-                {
-                    int length = examination[i].Length - 1;
-                    if (length <= 3)
-                    {
-                        Console.WriteLine("Слова в которых не больше х символов - " + words[i]);
-                        Console.WriteLine("");
-                        counter++;
-                        var = words[i];
-                        
-                    }
-                    
-               }
+    Console.WriteLine("");
 
     string[] solution = new string[counter];
-        Console.WriteLine("Размер массива solution равен " + solution.Length);
-        for( int i = 0; i < solution.Length; i++)
-            {
-                solution[i] = var;
-                Console.WriteLine(solution[i]);
-            }
-        
+    Console.WriteLine("Размер массива solution равен " + solution.Length);
+    for (int i = 0; i < words.Length; i++)
+    {
+        int length = words[i].Length - 1;
+        if (length <= 3)
+        {                        
+            int y = 0;
+            solution[y] = words[i];
+            Console.Write(solution[y]);
+            y++;                        
+        }
+                
+    }
 
-            
-        
-   
+    
+
 }
 task1();
